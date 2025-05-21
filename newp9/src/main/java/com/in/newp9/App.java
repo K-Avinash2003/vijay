08d2b6cc48d875b66af40c2ce728b42ce3fc929b
Manohari17 <manoharisannakki@gmai.com>
2025-05-21 09:43:47 +0530

    first commit

---
+++ newp9/src/main/java/com/in/newp9/App.java
@@ -0,0 +1,13 @@
+package com.in.newp9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
